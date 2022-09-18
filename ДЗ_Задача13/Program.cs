fd91e1f95c44d7e2fc800ab800818f89e774f2a4
LA <annleli@yandex.ru>
2022-09-18 15:20:22 +0300

    task 13

---
+++ ДЗ_Задача13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введи любое число ");
+string n = Convert.ToString(Console.ReadLine());
+
+if (n.Length > 2)
+
+    Console.WriteLine(n[2]);
+else
+    Console.Write("третьей цифры нет");
+
