eab3e28b715c5f706716730b2cb94761d9d86837
LA <annleli@yandex.ru>
2022-09-20 17:26:53 +0300

    task 15

---
+++ ДЗ_Задача15/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введи порядковый номер дня недели ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+while (n < 1 || n > 7)
+{
+    Console.Write("Нет такого дня недели. Введи порядковый номер от 1 до 7: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n == 6 || n == 7)
+     Console.Write("Yes");
+else 
+    Console.Write("No");
