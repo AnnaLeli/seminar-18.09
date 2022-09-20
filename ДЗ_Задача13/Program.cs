b2e7b4c45d0220009e8b1523b57893174d5c4d88
LA <annleli@yandex.ru>
2022-09-20 17:30:32 +0300

    task 13

--- ДЗ_Задача13/Program.cs
+++ ДЗ_Задача13/Program.cs
@@ -1,10 +1,28 @@
-﻿Console.Clear();
+﻿
+/* Первый вариант решения
+Console.Clear();
 Console.Write("Введи любое число ");
 string n = Convert.ToString(Console.ReadLine());
-
 if (n.Length > 2)
 
     Console.WriteLine(n[2]);
 else
     Console.Write("третьей цифры нет");
 
+*/
+Console.Clear();
+Console.Write("Введи любое число ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 100)
+    Console.Write("третьей цифры нет");
+else
+{
+    do 
+{
+     n = n / 10;
+}
+while (n > 999);
+int n1 = n % 10;
+
+ Console.Write("Третья цифра " + n1);
+ }
