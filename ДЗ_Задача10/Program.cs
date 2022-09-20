9d9ab0bce2241b9c4ac1e69daa74b04c126b1193
LA <annleli@yandex.ru>
2022-09-20 17:30:19 +0300

    task 10

--- ДЗ_Задача10/Program.cs
+++ ДЗ_Задача10/Program.cs
@@ -1,14 +1,14 @@
 ﻿Console.Clear();
-Console.Write("Введи трехзначное число ");
 
-{
-    while (Convert.ToInt32(Console.ReadLine()) < 100 || Convert.ToInt32(Console.ReadLine()) > 999)
-{
-    Console.Write("Это не трехначное число! ");
-    break;
-}
+Console.Write("Введи трехзначное число ");
 int n = Convert.ToInt32(Console.ReadLine());
-int n1 = n / 10;
-int n2 = n1 % 10;
-Console.Write(n2);
+
+while (n < 99 || n  > 1000)
+{
+    Console.Write("Это не трехначное число! Попробуй еще раз: ");
+    n = Convert.ToInt32(Console.ReadLine());
 }
+    int n1 = n / 10;
+    int n2 = n1 % 10;
+    Console.Write(n2); 
+
